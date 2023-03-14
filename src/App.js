4ff92332bd402829a1c62630c547d14b165760a5
achrafaitmbarek <the-crazy-achraf@hotmail.fr>
2023-03-14 05:37:10 +0100

    adding search searched components and updating cuisine home viggie ones

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import './App.css';
 import Category from './components/Category';
 import Pages from './pages/Pages';
-import {BrowserRouter} from 'react-router-dom';
+import {BrowserRouter, Link} from 'react-router-dom';
 import Search from './components/Search';
+import styled from 'styled-components';
+import {GiKnifeFork} from "react-icons/gi";
 
 
 function App() {
   return (
     <div className="App">
      <BrowserRouter>
+            <Nav/>
+            <GiKnifeFork/>
+            <Logo to={"/"}>Food Land</Logo>
             <Search/>
             <Category/>
             <Pages/>
@@ -17,5 +22,22 @@ function App() {
     </div>
   );
 }
+
+const Logo =styled(Link)`
+  text-decoration: none;
+  font-size: 1.5rem;
+  font-weight: 400;
+  font-family: 'Lobster Two',cursive;
+`;
+
+const Nav = styled.div`
+  padding:4rem 0rem;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  svg{
+    font-size: 2rem;
+  }
+`
 
 export default App;
